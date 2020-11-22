33bb62b9f809996add78ddbd2c225b38b45c6f6b
wuhantao <wuhantao1216@gmail.com>
2020-11-22 16:52:54 +0800

    inerest

---
+++ interest/cmake-build-debug/CMakeFiles/interest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interest.dir/main.c.o"
+  "interest.pdb"
+  "interest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/interest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
